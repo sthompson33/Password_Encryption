d633972b457809b4d70ceeeae87de3f2c3278db9
Stephen Thompson <thompson.stephen33@gmail.com>
2016-12-30 19:50:05 -0500

    default constructors added, start on main

--- PasswordEncryption/Account.h
+++ PasswordEncryption/Account.h
@@ -7,11 +7,27 @@ private:
 	string username;
 	string password;
 public:
+	Account();
+	Account(string, string);
 	void setUsername(string);
 	void setPassword(string);
 	string getUsername();
 	string getPassword();
 };
+
+//constructor
+Account::Account() {
+	
+	cout << "Username: ";
+	getline(cin, username);
+	cout << "Password: ";
+	getline(cin, password);
+}
+
+Account::Account(string u, string p) {
+	username = u;
+	password = p;
+}
 
 void Account::setUsername(string u) {
 	username = u;
--- PasswordEncryption/Main.cpp
+++ PasswordEncryption/Main.cpp
@@ -3,10 +3,20 @@
 */
 #include<iostream>
 #include<string>
+#include<fstream>
 #include"Account.h"
 using namespace std;
 
 int main() {
 
+	char choice;
+	cout << "1. Create New Account" << endl;
+	cout << "2. Sign In" << endl;
+	cout << ">> ";
+	cin >> choice;
+	cin.ignore();
+	//create an Account object using default constructor
+	Account user;
+	
 	return 0;
 }
